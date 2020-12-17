cc5bcdade67a4f4aeff11293a326cb09811325f0
fangxiaoran <740384523@qq.com>
2020-12-17 18:14:04 +0800

    添加到本地仓库ideanproject第一次

---
+++ src/com/xms/entity.java
@@ -0,0 +1,34 @@
+package com.xms;
+
+/**
+ * @author fangxiaoran
+ * @create 2020 - 12 - 16  22:17
+ **/
+public class entity {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 5+5;
+        System.out.println(a==b);
+        System.out.println("hello");
+        System.out.println("fangxiaoran");
+    }
+}
